5411a1d2463661d1fab49251d814332a8a20c8dd
martdan <asrafdanial12@gmail.com>
2024-10-27 22:19:45 +0800

    firebase initialisation

---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, setPersistence, browserSessionPersistence } from "firebase/auth"
+import { getStorage } from "firebase/storage"
+import { getFirestore } from "firebase/firestore"
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const storage = getStorage(app);
+export const db = getFirestore(app);
+
+setPersistence(auth, browserSessionPersistence)
+    .then(() => {
+        console.log('Session persistence set to browser session');
+    })
+    .catch((error) => {
+        console.error('Error setting session persistence:', error);
+    });
+
+export { auth, storage };
